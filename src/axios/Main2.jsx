8dd434b5ffb16c941ee47f3480c62479279eb4c9
arya <gulshansarya9@gmail.com>
2023-02-25 01:05:07 +0530

    update search and sort method

--- src/axios/Main2.jsx
+++ src/axios/Main2.jsx
@@ -2,33 +2,68 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./main.css"
 
-const Main2= ()=> {
+const Main2 = ()=> {
 const [data, setData]=useState([]);
+const [searchApiData,setSearchApiData] = useState([]);
+const [sortasc, setSortAsc]=useState([]);
+const [sortdsc, setSortDsc]=useState([]);
+const [filterVal, setFilterVal]=useState('');
 
 useEffect(() => {
-  GetRandomData();
+  const GetRandomData = async()=>{
+    const result = await axios("https://dummyjson.com/products");
+     if(result){
+   setData(result.data.products)
+   setSearchApiData(result.data.products)
+     }
+   }
+   GetRandomData();
 }, []);
 
-const GetRandomData= ()=>{
- axios.get("https://dummyjson.com/products")
- .then((result)=>{setData(result.data.results);})
+const numAscending = ()=>{
+const sortdataA = data.sort((a, b) =>{
+  return  a.price-b.price
+ });
+ setSortAsc({sortasc:sortdataA})
 }
 
-const numAscending =()=> [...data].sort((a, b) => a.price- b.price);
-const numDescending =()=> [...data].sort((a, b) => b.price-a.price );
+const numDescending =()=> {
+const sortdataD = data.sort((a, b) =>{
+  return b.price-a.price;
+});
+setSortDsc({sortdsc:sortdataD})
+}
+
+const handleFilter = (e)=>{
+  if(e.target.value==''){
+    setData(searchApiData)
+  }
+  else{
+  const filterdata = searchApiData.filter((item)=>item.title.toLowerCase().includes(e.target.value.toLowerCase()));
+  setData(filterdata);
+}
+setFilterVal(e.target.value)
+}
 
 if (!data.length) return <h3>Loading...</h3>;
 
 return (
  <div className="row">
-  <div>
-    <h3>Sort</h3>
-  <button onClick={()=>numAscending()}>Low to Hight</button>
-  <button onClick={()=>numDescending()}>High to Low</button>
+  <div className="button">
+    <div className="sort">
+    <span><h2>Sort By :</h2></span>
+    <span><button className="btn1" onClick={()=>numAscending()}>Low-High</button></span>
+    <span><button className="btn2" onClick={()=>numDescending()}>High-Low</button></span> 
+    </div>
+    <div className="search">
+    <input type="search" placeholder="search title"  value={filterVal}
+      onChange={(e)=>handleFilter(e)} />
+    </div>
   </div>
-  
-     {data.map((item,i) => (
-      <div className="column" key={i}>
+  <div>
+     {data ? 
+     data.map((item,index) => (
+      <div className="column" key={index}>
       <img src={item.thumbnail} alt={item.thumbnail}></img>
        <h2> Title: {item.title} </h2>
        <p> Price: {item.price}</p>
@@ -36,10 +71,9 @@ return (
        <p> Category: {item.category}</p>
        <p> Description: {item.description}</p>
       </div>
-       
-     ))}
+      )):"No Data Found"}
+     </div>
   </div>
-
 );
 }
 export default Main2;
